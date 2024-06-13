ea35fc3ca117ec239182b510280139b3a6a20159
jiangziyan-693 <ziy.jiang@outlook.com>
2024-06-13 22:32:48 +0800

    Compiler.cpp-main函数解析

--- compiler2023-202310006201934-submit/src/Compiler.cpp
+++ compiler2023-202310006201934-submit/src/Compiler.cpp
@@ -27,6 +27,7 @@
 std::string input_file, ir_file, target_file;
 int optimize_level;
 
+//用于解析命令行参数，并将相应的参数值赋给全局变量 input_file、ir_file、target_file 和 optimize_level
 void parse_args(int argc, char *argv[]) {
     // 1. ./compiler -S -o <target_file> <input_file>
     // 2. ./compiler -S -o <target_file> <input_file> -O1
@@ -63,6 +64,8 @@ void parse_args(int argc, char *argv[]) {
 
 /* Argument Parse end */
 
+//backend函数是编译器后端的主要入口，它开始记录代码生成的开始时间，然后调用CodeGen::CODEGEN->gen()来生成目标代码，之后，它会记录代码生成所用的时间并输出
+//接下来，会根据优化级别（optimize_level)执行一系列优化，包括常量折叠，无用指令删除，寄存器分配，指令合并等。
 void backend() {
     // CenterControl::_FAST_REG_ALLOCATE = true;
     std::cerr << "code gen begin" << std::endl;
@@ -191,42 +194,63 @@ void backend() {
     }
 }
 
+//程序的入口，它首先调用 parse_args 解析命令行参数，然后设置编译器的优化级别。
+//它调用 FileDealer::inputDealer 处理输入文件，并执行词法分析（lexer.lex()）和语法分析（parser.parseAst()）。
+//解析完语法树后，它会调用 Visitor 来访问 AST 并生成中间表示。
 int main(int argc, char *argv[]) {
     // std::cerr << CodeGen::floatCanCode(0.0f) << std::endl;
     // return 0;
+
+    //解析命令行参数
     parse_args(argc, argv);
     if (optimize_level > 0) {
         CenterControl::_O2 = true;
     }
+
+    //处理输入文件
     FileDealer::inputDealer(input_file.c_str());
+
+    //创建词法分析器并进行词法分析
     Lexer lexer = Lexer();
     lexer.lex();
+
+    //创建语法分析器并进行语法分析
     Parser parser = Parser(lexer.tokenList);
+    //记录开始时间
     std::cerr << "Parser & Visitor begin" << std::endl;
     auto start = std::chrono::high_resolution_clock::now();
+
+    //生成抽象语法树
     AST::Ast *ast = parser.parseAst();
+    //访问抽象语法树并生成中间表示
     Visitor visitor = Visitor();
     visitor.visitAst(ast);
+    //记录并输出解析和访问抽象语法树所用时间
     auto end = std::chrono::high_resolution_clock::now();
-    std::cerr << "Parser & Visitor end, Use Time: " << std::chrono::duration<double>(end - start).count() << "s"
-              << std::endl;
+    std::cerr << "Parser & Visitor end, Use Time: " << std::chrono::duration<double>(end - start).count() << "s"<< std::endl;
 
+    //输出LLVM中间表示（IR)
     Manager::MANAGER->outputLLVM("llvm-0.ll");
 
+    //开始中间代码运行和优化
     std::cerr << "MidEndRunner begin" << std::endl;
     start = std::chrono::high_resolution_clock::now();
     MidEndRunner *midEndRunner = new MidEndRunner((Manager::MANAGER->getFunctionList()));
     midEndRunner->Run(optimize_level);
     end = std::chrono::high_resolution_clock::now();
+    //记录并输出中间代码运行和优化时间
     std::cerr << "MidEndRunner end, Use Time: " << std::chrono::duration<double>(end - start).count() << "s"
               << std::endl;
 
+    //再次输出LLVM中间表示（优化后）
     Manager::MANAGER->outputLLVM(ir_file.empty() ? "llvm-1.ll" : ir_file);
 
+    //如果没有指定目标文件，则只输出前端结果
     if (target_file.empty()) {
         std::cerr << "Frontend Only" << std::endl;
         return 0;
     }
+    //如果有优化选项，则执行额外优化步骤
     if (CenterControl::_O2) {
         RemovePhi *removePhi = new RemovePhi(midEndRunner->functions);
         removePhi->Run();
@@ -241,8 +265,11 @@ int main(int argc, char *argv[]) {
         Manager::MANAGER->outputLLVM("llvm-3.ll");
     }
 
+    //执行后端处理，生成目标代码
     backend();
-
+    //输出最终的目标文件
     McProgram::MC_PROGRAM->output(target_file);
+
+    //程序结束
     return 0;
 }
